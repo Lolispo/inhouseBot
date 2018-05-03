07c0f8f9852c90bee5fbd01549c328657ca496c0
Petter Andersson <petterandersson95@gmail.com>
2018-05-03 20:08:44 +0200

    added !active command to check active users in voice channels

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Discord = require('discord.js');
 
 // Get Instance of discord client
@@ -15,13 +17,28 @@ client.on('message', message => {
   if(message.content == 'hej'){
     message.channel.send('lul')
   }
-  if(message.content === `${prefix}ping`){
+  if(message.content === prefix+'ping'){
   	console.log('PingAlert, user had !ping as command');
   	message.channel.send('Pong');
+  }
+  if(message.content === `${prefix}active`){
+  	var channels = message.guild.channels;
+  	channels.forEach(function(channel, id) {
+	   if(channel.type === "voice"){
+	   		console.log("VoiceChannel", channel.name, " (id =",channel.id,") active users: (Total: ", channel.members.size ,")");
+	   		channel.members.forEach(function(members){
+	   			if(!members.bot){ // Får endast riktiga users
+					console.log("\t", members.user.username, "(", members.user.id, ")"); // Printar alla activa users i denna voice chatt
+	   			}
+	   		});
+	   }
+	})
   }
 });
 
 //Create more events to do fancy stuff with discord API
 
+
 //login
 client.login(token);
+
