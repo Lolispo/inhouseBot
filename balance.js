da92ca52422ba0e11c57298d323b6a9953ca2fa2
Petter Andersson <petterandersson95@gmail.com>
2018-05-04 03:21:51 +0200

    Added TeamCombination system to get best teams, with working test code to work with

---
+++ balance.js
@@ -0,0 +1,205 @@
+'use strict;'
+
+const ArrayList = require('arraylist');
+const moment = require('moment');
+
+/*
+ 	TODO Big:
+ 		Database for saving data connected to user
+ 			Can use sequelize as in lab, store ID | MMR
+ 		Refactor ugly solutions
+		Implement MMR Update (Req mmr save for full implementation)
+			ELO math theory could be looked at
+*/
+// @param players should contain ArrayList of initialized Players of people playing
+exports.balanceTeams = function(players){
+	// console.log('DEBUG: @balanceTeams');
+	// Generate team combs, all possibilities of the 10 players
+	var teamCombs = generateTeamCombs(players);
+	
+	var result = findBestTeamComb(players, teamCombs);
+
+	var stringToReturn = buildReturnString(result);
+	// Return string to message to clients
+	console.log(stringToReturn);
+	return stringToReturn;
+}
+
+// Build a string to return to print as message
+function buildReturnString(obj){ // TODO: Make print consistently nice
+	// Check if adjustment is needed
+	var date = moment().format('LLL');
+	var s = '';
+	s += 'MMR Average difference: ' + obj.avgDiff + ' (Total: ' + obj.difference + 'p). ';
+	s += String(date);
+	s += '\n';
+	s += 'T1: ' + obj.team1[0].userName + ' (' + obj.team1[0].mmr + ')';
+	for(var i = 1; i < obj.team1.length; i++){
+		s += ',\t' + obj.team1[i].userName + ' (' + obj.team1[i].mmr + ')';
+	}	
+	s += '. \tAvg: ' + obj.avgT1;
+	s += '\n';
+	s += 'T2: ' + obj.team2[0].userName + ' (' + obj.team2[0].mmr + ')';
+	for(var i = 1; i < obj.team2.length; i++){
+		s += ',\t' + obj.team2[i].userName + ' (' + obj.team2[i].mmr + ')';
+	}
+	s += '. \tAvg: ' + obj.avgT2;
+	s += '\n';
+	return s;
+}
+
+// TODO Refactor: Should make less repetitive code
+// Generates the combinations for different team sizes
+// uniqueCombs makes sure that duplicates aren't saved (Unsure if uniqueSum even helps, should in theory)
+// @return teamCombs is returned with all possible matchups
+function generateTeamCombs(players){
+	var teamCombs = [];   // Saves all team combination, as arrays of indexes of 5 (other team is implied)
+	var uniqueCombs = new ArrayList; // A number combination for each comb, to prevent saving duplicates.
+	var len = players.size();
+	for(var i = 0; i < len; i++){
+		for(var j = i+1; j < len; j++){
+			for(var k = j+1; k < len; k++){
+				for(var l = k+1; l < len; l++){
+					for(var m = l+1; m < len; m++){
+						if(len === 10){
+							var uniqueSum = uniVal(i) + uniVal(j) + uniVal(k) + uniVal(l) + uniVal(m); 
+							if(!uniqueCombs.contains(uniqueSum)){	
+								var teamComb = [i,j,k,l,m]
+								teamCombs.push(teamComb); // Add new combination to teamCombs // [i,j,k,l,m]
+								uniqueCombs.add(uniqueSum);
+							} 	
+						}					
+					}
+					if(len === 8){
+						var uniqueSum = uniVal(i) + uniVal(j) + uniVal(k) + uniVal(l); 
+						if(!uniqueCombs.contains(uniqueSum)){	
+							var teamComb = [i,j,k,l]
+							teamCombs.push(teamComb); // Add new combination to teamCombs // [i,j,k,l]
+							uniqueCombs.add(uniqueSum);
+						} 	
+					}
+				}
+				if(len === 6){
+					var uniqueSum = uniVal(i) + uniVal(j) + uniVal(k); 
+					if(!uniqueCombs.contains(uniqueSum)){	
+						var teamComb = [i,j,k]
+						teamCombs.push(teamComb); // Add new combination to teamCombs // [i,j,k]
+						uniqueCombs.add(uniqueSum);
+					} 	
+				}
+			}
+			if(len === 4){
+				var uniqueSum = uniVal(i) + uniVal(j); 
+				if(!uniqueCombs.contains(uniqueSum)){	
+					var teamComb = [i,j]
+					teamCombs.push(teamComb); // Add new combination to teamCombs // [i,j]
+					uniqueCombs.add(uniqueSum);
+				} 	
+			}
+		}
+	}
+
+	//console.log('DEBUG: @generateTeamCombs, teamCombs = ', teamCombs, teamCombs.length, uniqueCombs, uniqueCombs.size());
+	return teamCombs;
+}
+
+function findBestTeamComb(players, teamCombs){
+	// Compare elo matchup between teamCombinations, lowest difference wins
+	var bestPossibleTeamComb = Number.MAX_VALUE;
+	var t1 = [];
+	var t2 = [];
+	var avgTeam1 = -1;
+	var avgTeam2 = -1;
+	var index = -1;
+	for(var i = 0; i < teamCombs.length; i++){
+		var teams = getBothTeams(teamCombs[i], players);
+		//console.log('DEBUG: @findBestTeamComb, getBothTeams = ', teams);
+		var res = mmrCompare(teams.t1, teams.t2);
+		//console.log('DEBUG: @findBestTeamComb, mmrCompare = ', res);
+		if(res.diff < bestPossibleTeamComb){
+			bestPossibleTeamComb = res.diff;
+			avgTeam1 = res.avgT1;
+			avgTeam2 = res.avgT2;
+			t1 = teams.t1;
+			t2 = teams.t2;
+			index = i;
+		}
+	}
+
+	// Retrieved most fair teamComb
+	return {team1 : t1, team2 : t2, difference : bestPossibleTeamComb, avgT1 : avgTeam1, avgT2 : avgTeam2, avgDiff : Math.abs(avgTeam1 - avgTeam2).toFixed(2)}; // Remove teamComb
+}
+
+// Unique number combinations for combinations of 5.
+// Should give (check): 0: 0, 1: 10, 2: 200, 3: 3000, 4: 40000, 5: 5, 6: 60, 7: 700, 8: 8000; 9: 90000
+// TODO Test
+function uniVal(x){
+	return (x * Math.pow(10, (x % 5)));
+}
+
+// Get the two teams of players from the teamComb
+function getBothTeams(teamComb, players){
+	var team1 = new ArrayList;
+	var team2 = new ArrayList;
+	for(var i = 0; i < players.size(); i++){
+		var contains = false;
+		for(var j = 0; j < teamComb.length; j++){ // TODO Refactor/Redo. Do better check for this contain check, teamComb = list ([])
+			if(i === teamComb[j]){
+				contains = true;
+			}
+		}
+		if(contains){
+			team1.add(players[i]);
+		} else {
+			team2.add(players[i]);
+		}
+	}
+	//console.log('DEBUG: @getBothTeams, team1 = ', team1, '\nteam2 = ', team2, 'teamComb', teamComb);
+	return {t1 : team1, t2 : team2};
+}
+
+// @param two teams of players
+// @return total mmr difference
+function mmrCompare(t1, t2){
+	var avgTeam1 = addTeamMMR(t1); 
+	var avgTeam2 = addTeamMMR(t2); 
+	var difference = Math.abs(avgTeam1 - avgTeam2);
+	return {diff : difference, avgT1 : (avgTeam1 / t1.length), avgT2 : (avgTeam2 / t2.length)};
+}
+
+function addTeamMMR(team){ // Function to be used in summing over players
+	var sum = 0;
+	for(var i = 0; i < team.length; i++){
+		sum += team[i].mmr;
+	}
+	//console.log('DEBUG: @addTeamMMR, team = ', team, 'TeamMMR:', sum);
+	return sum;
+}
+
+
+function Player(username, discId){
+	this.userName = username;
+	this.discID = discId;
+	this.defaultMMR = 1000; // TODO: Adjust to elo system
+	this.mmr; 
+	// Check if existing mmr exist for discID in database, otherwise set as default mmr
+	if( false ){ // <- Check in db
+		// TODO
+	} else{
+		this.mmr = this.defaultMMR;
+	}
+
+	this.setMMR = function(value){
+		this.mmr = value;
+	}
+}
+
+exports.createTempPlayer = function(username, discId, mmr){
+	var player = new Player(username, discId);
+	player.setMMR(mmr);
+	return player;
+}
+
+exports.createPlayer = function(username, discId){
+	return new Player(username, discId);
+}
--- bot.js
+++ bot.js
@@ -5,35 +5,111 @@ const Discord = require('discord.js');
 // Get Instance of discord client
 const client = new Discord.Client();
 
+const ArrayList = require('arraylist');
+const balance = require('./balance');
+
 //get config data
 const { prefix, token } = require('./conf.json');
+
+/*
+	TODO: 
+		Better Printout / message to clients (Currently as message, but not nice looking) (Deluxe: maybe image if possible)
+		better names for commands
+
+	Deluxe: 
+		Move players into separate channels on team generation
+		Remove player written message after a couple of seconds (at least for testing purpose) to prevent chat over flooding
+*/
+
 // will only do stuff after it's ready
 client.on('ready', () => {
-  console.log('ready to rumble');
+	console.log('ready to rumble');
 });
 
 client.on('message', message => {
-  console.log('MSG: ', message.content);
-  if(message.content == 'hej'){
-    message.channel.send('lul')
-  }
-  if(message.content === prefix+'ping'){
-  	console.log('PingAlert, user had !ping as command');
-  	message.channel.send('Pong');
-  }
-  if(message.content === `${prefix}active`){
-  	var channels = message.guild.channels;
-  	channels.forEach(function(channel, id) {
-	   if(channel.type === "voice"){
-	   		console.log("VoiceChannel", channel.name, " (id =",channel.id,") active users: (Total: ", channel.members.size ,")");
-	   		channel.members.forEach(function(members){
-	   			if(!members.bot){ // Får endast riktiga users
-					console.log("\t", members.user.username, "(", members.user.id, ")"); // Printar alla activa users i denna voice chatt
-	   			}
-	   		});
-	   }
-	})
-  }
+	console.log('MSG: ', message.content);
+	if(message.content == 'hej'){
+		message.channel.send('lul')
+	}
+	if(message.content === prefix+'ping'){
+		console.log('PingAlert, user had !ping as command');
+		message.channel.send('Pong');
+	}
+	if(message.content === `${prefix}active`){
+		var channels = message.guild.channels;
+		//console.log(message); // Can print for information about hierarchy in discord message
+		channels.forEach(function(channel, id) {
+			if(channel.type === 'voice'){
+				console.log('VoiceChannel', channel.name, ' (id =',channel.id,') active users: (Total: ', channel.members.size ,')');
+				var correctChannel = false;
+				var playerInformation = new ArrayList;
+				channel.members.forEach(function(members){
+					if(!members.bot){ // Only real users
+						console.log('\t', members.user.username, '(', members.user.id, ')'); // Printar alla activa users i denna voice chatt
+						playerInformation.add({id : members.user.id, userName : members.user.userName});
+						if(members.id === message.author.id){ // Same voice channel as the author of the message
+							correctChannel = true;
+						}
+					}
+				});
+				if(correctChannel){ // Act on this channel if it is correct one
+					console.log('Go to test: ', playerInformation.size(), message.author.username);
+					if(playerInformation.size() == 10){// Check amount of users
+						// initalize 10 Player objects with playerInformation
+						var players = new ArrayList;
+						for(var i = 0; i < playerInformation.size(); i++){
+							var tempPlayer = balance.createPlayer(playerInformation[i].userName, playerInformation[i].id);
+							players.add(tempPlayer);
+						}
+						var result = balanceTeams(players); // Initialize balancing, return result
+						// Show result to discord users
+						message.channel.send(result);
+					} else if(playerInformation.size() === 1 && (message.author.username === 'Petter' || message.author.username === 'Obtained') ){
+						console.log('Testing Environment: Generating 10 players with default mmr to balance');
+						var players = new ArrayList;
+						for(var i = 0; i < 10; i++){
+							var tempPlayer = balance.createTempPlayer('Player ' + i, i, 900 + ( Math.floor(Math.random() * Math.floor(200))) );
+							//console.log('DEBUG: @!active, player[' + i + '] =', tempPlayer);
+							players.add(tempPlayer);
+						}
+						var result = balance.balanceTeams(players); // Initialize balancing, return result
+						// Show result to discord users
+
+						players = new ArrayList;
+						for(var i = 0; i < 8; i++){
+							var tempPlayer = balance.createTempPlayer('Player ' + i, i, 900 + ( Math.floor(Math.random() * Math.floor(200))) );
+							//console.log('DEBUG: @!active, player[' + i + '] =', tempPlayer);
+							players.add(tempPlayer);
+						}
+						var result = balance.balanceTeams(players);
+
+						players = new ArrayList;
+						for(var i = 0; i < 6; i++){
+							var tempPlayer = balance.createTempPlayer('Player ' + i, i, 900 + ( Math.floor(Math.random() * Math.floor(200))) );
+							//console.log('DEBUG: @!active, player[' + i + '] =', tempPlayer);
+							players.add(tempPlayer);
+						}
+						var result = balance.balanceTeams(players);
+
+						players = new ArrayList;
+						for(var i = 0; i < 4; i++){
+							var tempPlayer = balance.createTempPlayer('Player ' + i, i, 900 + ( Math.floor(Math.random() * Math.floor(200))) );
+							//console.log('DEBUG: @!active, player[' + i + '] =', tempPlayer);
+							players.add(tempPlayer);
+						}
+						var result = balance.balanceTeams(players);
+
+						//message.channel.send(result);
+					} else{
+						console.log('Currently only support 10 players'); // TODO, support more sizes
+					}
+				}
+			}
+		})
+	}
+
+	// TODO Feat: Add functionality to remove player written message after ~5 sec, prevent flooding
+
 });
 
 //Create more events to do fancy stuff with discord API
@@ -41,4 +117,3 @@ client.on('message', message => {
 
 //login
 client.login(token);
-
