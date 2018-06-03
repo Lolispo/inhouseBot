00d82c65499cc545e644a5d86d367d60091d6a37
Petter Andersson <petterandersson95@gmail.com>
2018-06-03 21:33:27 +0200

    adding game table

--- test.sql
+++ test.sql
@@ -1,11 +1,4 @@
-CREATE TABLE ratings(
-	uid VARCHAR(64) NOT NULL,
+CREATE TABLE game(
 	gameName VARCHAR(64) NOT NULL,
-	mmr int,
-	gamesPlayed int,
-	wins int,
-	losses int,  
-	PRIMARY KEY (uid, gameName),
-	FOREIGN KEY (uid) REFERENCES users(uid),
-	FOREIGN KEY (gameName) REFERENCES game(gameName)
+	PRIMARY KEY (gameName)
 );
