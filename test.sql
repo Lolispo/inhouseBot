9f929a303f4128ff92d7d1417b084f891808c605
Petter Andersson <petterandersson95@gmail.com>
2018-06-03 21:45:44 +0200

    attempt 2

--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
 use pettea;
 
-INSERT INTO ratings (uid, gameName, mmr, gamesPlayed, wins, losses) VALUES ((SELECT uid FROM users), 'cs', (SELECT cs FROM users), (SELECT gamesPlayed FROM users), 0, 0);
+INSERT INTO ratings (uid, gameName, mmr, gamesPlayed, wins, losses) VALUES ((SELECT uid FROM users where userName = 'Petter'), 'cs', (SELECT cs FROM users where userName = 'Petter'), (SELECT gamesPlayed FROM users where userName = 'Petter'), 0, 0);
 
 /*
 CREATE TABLE game(
