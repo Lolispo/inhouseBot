451cd5f7af851bddf4547f60629bd2a8ed4f077c
Petter Andersson <petterandersson95@gmail.com>
2018-10-27 15:49:54 +0200

    Run tests in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+echo "Getting latest version from Github ..."
 git pull
+echo "Running tests ..."
+npm test
 echo "Starting bot, printing output to LOG files"
 npm start >> OUTPUT.log 2>ERROR.log
+# To show Output easy, 'tail -n 50 OUTPUT.log'
