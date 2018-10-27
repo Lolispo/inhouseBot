d5a00667b16cb3807af2dbcde79db7a4cd85ae43
Petter Andersson <petterandersson95@gmail.com>
2018-10-27 15:41:02 +0200

    Added test cases for reverseUniquesum and recursivefor

--- test/bot_test.js
+++ test/bot_test.js
@@ -4,11 +4,12 @@
 var assert = require('assert');
 var rewire = require("rewire");
 
-var myModule = rewire("../src/balance.js");
+var balanceModule = rewire("../src/balance.js");
 // Test method 
 
-var recursiveFor = myModule.__get__('recursiveFor')
-var unival = myModule.__get__('uniVal');
+var recursiveFor = balanceModule.__get__('recursiveFor')
+var unival = balanceModule.__get__('uniVal');
+var reverseUniqueSum = balanceModule.__get__('reverseUniqueSum');
 
 describe('balance', function(){
     describe('unival', function(){
@@ -27,22 +28,48 @@ describe('balance', function(){
             assert.equal(10, unival(10));
             assert.equal(110, unival(11));
             assert.equal(1200, unival(12));
-            assert.equal(14000, unival(13));
-            assert.equal(150000, unival(14));
+            assert.equal(13000, unival(13));
+            assert.equal(140000, unival(14));
         });
     });
-    /*
+    
+    describe('reverseUniqueSum', function(){
+        it('should give correct result for reverse', function(){
+            var array = [0, 1]; // unival(2) + unival(3)
+            assert.equal(3200, reverseUniqueSum(array, array.length * 2));
+            var array = [2, 3];
+            assert.equal(10, reverseUniqueSum(array, array.length * 2));
+            var array = [0,1,2,3,4];
+            assert.equal(98765, reverseUniqueSum(array, array.length * 2));
+        });
+    });
+
     describe('recursiveFor', function(){
-        it('should give  on size 4', function(){
+        // Should give half of size/halfsize, 0.5 * choosing 2 from 4 => 3
+        // Set size are all unique combinations, contains the double ones from reverseUniqueSum => double amount
+        it('should give 3 teamCombs on size 4', function(){
+            var array = [];
+            var set = new Set();
+            recursiveFor(0, [], 4, 0, array, set);
+            assert.equal(3, array.length);
+            assert.equal(6, set.size);
+        });
+        it('should give 126 teamCombs on size 10', function(){
             var array = [];
             var set = new Set();
             recursiveFor(0, [], 10, 0, array, set);
-            //assert.equal(array, array.length === 4);
-            console.log(array);
-            //console.log(set);
+            assert.equal(126, array.length);
+            assert.equal(252, set.size);
+        });
+        it('should give 35 teamCombs on size 8', function(){
+            var array = [];
+            var set = new Set();
+            recursiveFor(0, [], 8, 0, array, set);
+            assert.equal(35, array.length);
+            assert.equal(70, set.size);
         });
     });
-    */
+    
 });
 describe('Test', function(){
     it('is 3*3 = 9?', function(){
