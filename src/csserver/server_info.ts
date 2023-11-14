df3b01cb435d7ecd96a1f0459012f65bbc9e3a45
Petter Andersson <petterandersson95@gmail.com>
2023-11-14 22:53:04 +0100

    cs ip

--- src/csserver/server_info.ts
+++ src/csserver/server_info.ts
@@ -1,5 +1,5 @@
-const csDomain = 'kosatupp.datho.st';
-const csPort = '28967';
+const csDomain = 'petter.datho.st';
+const csPort = '26093';
 const csPw = 'get';
 let csConnectConsole = `connect ${csDomain}:${csPort}; password ${csPw}`; // connect kosatupp.datho.st:27207; password get
 let csConnectUrl = `steam://connect/${csDomain}:${csPort}/${csPw}`; 			// steam://connect/kosatupp.datho.st:28967/get
